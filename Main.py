c57b72571ff45006074b027e33f33c7fbcbff018
Svaught598 <svaught598@gmail.com>
2020-02-06 17:05:55 -0600

    added navigation drawer

--- Main.py
+++ Main.py
@@ -1,36 +1,61 @@
 import os 
 
-from kivy.app import App
+from kivymd.app import MDApp
+from kivymd.uix.button import MDFlatButton
+from kivymd.uix.list import OneLineAvatarListItem
+
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty
 from kivy.lang.builder import Builder
-from kivy.uix.screenmanager import ScreenManager
+
 from screens import VolumeScreen, UpdateScreen, FilmThicknessScreen
-from views.main_screen import MainScreen
-
-from settings import TEMPLATE_PATHS
-
-for path in TEMPLATE_PATHS:
-    Builder.load_file(path)
-
-class Manager(ScreenManager):
-    """Screenmanager for all children screens"""
-    def __init__(self, **kwargs):
-        super(Manager, self).__init__(**kwargs)
-        self.menu = MainScreen(name = 'menu')
-        self.add_widget(self.menu)
-        self.screen1 = VolumeScreen(name = 'volumeneeded')
-        self.add_widget(self.screen1)
-        self.screen2 = UpdateScreen(name = 'update')
-        self.add_widget(self.screen2)
-        self.screen3 = FilmThicknessScreen(name = 'thickness')
-        self.add_widget(self.screen3)
-        
+from settings import TEMPLATE_PATHS, MAIN_TEMPLATE_PATH
 
 
-class SolutionApp(App):
+class ContentNavigationDrawer(BoxLayout):
+    pass
+
+
+class NavigationItem(OneLineAvatarListItem):
+    icon = StringProperty()
+
+
+class SolutionApp(MDApp):
     
     def build(self):
-        return Manager()
+        for path in TEMPLATE_PATHS:
+            Builder.load_file(path)
+        return Builder.load_file(MAIN_TEMPLATE_PATH)
 
+    def on_start(self):
+        self.add_screens()
+        context = {
+            'Home': 'home-circle-outline',
+            'Settings': 'settings-outline',
+            'About': 'lambda',
+            'Developing': 'keyboard',
+            'Exit': 'exit-to-app',
+        }
+        for items in context.items():
+            self.root.ids.content_drawer.ids.box_item.add_widget(
+                NavigationItem(
+                text=items[0],
+                icon=items[1],
+                )
+            )
+
+    def add_screens(self):
+        self.screen1 = VolumeScreen(name = 'volumeneeded')
+        self.root.ids.screens.add_widget(self.screen1)
+        self.screen2 = UpdateScreen(name = 'update')
+        self.root.ids.screens.add_widget(self.screen2)
+        self.screen3 = FilmThicknessScreen(name = 'thickness')
+        self.root.ids.screens.add_widget(self.screen3)
+
+    def get_main_screen(self):
+        self.root.ids.screens.current = 'menu'
+        return 
     
 if __name__ == '__main__':
     SolutionApp().run()
--- settings.py
+++ settings.py
@@ -8,8 +8,10 @@ TEMPLATES_DIR = os.path.join(ROOT_DIR, 'templates')
 
 # List of paths for .kv files
 TEMPLATE_PATHS = [
-    os.path.join(ROOT_DIR, 'templates\\main_screen.kv'),
     os.path.join(ROOT_DIR, 'templates\\film_thickness_screen.kv'),
     os.path.join(ROOT_DIR, 'templates\\update_data_screen.kv'),
     os.path.join(ROOT_DIR, 'templates\\volume_needed_screen.kv'),
+    os.path.join(ROOT_DIR, 'templates\\navigation_drawer.kv'),
 ]
+
+MAIN_TEMPLATE_PATH = os.path.join(ROOT_DIR, 'templates\\main.kv')
