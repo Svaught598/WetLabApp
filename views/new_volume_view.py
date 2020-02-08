e7056e1201d6be46f970ebb3ccda75dc7465423c
Svaught598 <svaught598@gmail.com>
2020-02-08 13:19:58 -0600

    added density arg; scheduled prepare

--- views/new_volume_view.py
+++ views/new_volume_view.py
@@ -22,7 +22,7 @@ class VolumeScreen(Screen):
 
     def __init__(self, *args, **kwargs):
         super(VolumeScreen, self).__init__(*args, **kwargs)
-        self.prepare()
+        Clock.schedule_once(lambda x: self.prepare(), 0)
 
     def prepare(self):
         app = MDApp.get_running_app()
@@ -55,121 +55,13 @@ class VolumeScreen(Screen):
             'solvent': self.ids.solvent.text,
             'material': self.ids.material.text,
             'mass': self.ids.mass.text,
-            'concentration': self.ids.concentration.text
+            'concentration': self.ids.concentration.text,
+            'density': self.density.text,
         })
 
     def show_error_message(self, error_message):
         self.ids.volume_needed.text = error_message
 
     def show_volume_needed(self, volume_needed):
-        self.ids.volume_needed.text = error_message
+        self.ids.volume_needed.text = volume_needed
 
-"""
-class VolumeScreen(Screen):
-    types = StringProperty('')
-    solvs = StringProperty('')
-    solts = StringProperty('')
-    mass = StringProperty('')
-    conc = StringProperty('')
-    dens = StringProperty('')
-    volume = StringProperty('')
-
-    def __init__(self, **kwargs):
-        super(VolumeScreen, self).__init__(**kwargs)
-        Clock.schedule_once(lambda x: self.prepare(), 0)
-
-    def prepare(self):
-        app = App.get_running_app()
-        self.view_model = app.volume_view_model
-        self.view_model.bind(
-            volume = lambda instance, volume: self.display_volume(volume)
-        )
-
-"""
-class VolumeScreen(Screen):
-    types = StringProperty('')
-    solvs = StringProperty('')
-    solts = StringProperty('')
-    mass = StringProperty('')
-    conc = StringProperty('')
-    dens = StringProperty('')
-    volume = StringProperty('')
-
-    def __init__(self, **kwargs):
-        super(VolumeScreen, self).__init__(**kwargs)
-        Clock.schedule_once(lambda x: self.prepare(), 0)
-
-    def prepare(self):
-        app = App.get_running_app()
-        self.view_model = app.volume_view_model
-        self.view_model.bind(
-            volume = lambda instance, volume: self.display_volume(volume)
-        )
-
-    def display_volume(self, volume):
-        app = App.get_running_app()
-        app.root.ids.volume.text = volume
-        
-    def calculate(self):
-        print(self.conc)
-        if self.types == '% Wt/Wt':
-            if self.verify(check_density = True) == True:
-                self.volume = self.calculate_wtwt()
-            else: 
-                _, message = self.verify(check_density = True)
-                self.volume = self.error_message(message)
-        elif self.types == '% Wt/V':
-            if self.verify() == True:
-                self.volume = self.calculate_wtv()
-            else: 
-                _, message = self.verify()
-                self.volume = self.error_message(message)
-        else: 
-            self.volume = "Type of solution not specified"
-
-    def calculate_wtwt(self):
-        conc = float(self.conc)
-        mass = float(self.mass)
-        sdens= float(self.dens)
-        return str(round(((1 - conc) * mass)/(conc * sdens), 3)) + ' ml'            
-            
-    def calculate_wtv(self):
-        conc = float(self.conc)
-        mass = float(self.mass)
-        return str(round((mass/conc), 3))
-        
-    def verify(self, check_density = False):
-        message = []
-        if self.conc == '':
-            message.append("Concentration field is empty\n")
-        else: 
-            try: 
-                if float(self.conc) > 1:
-                    message.append("Concentration must be less than 1\n")
-            except:
-                message.append("Concentration must be greater than 0\n")
-    
-        if self.mass == '':
-            message.append("Mass field is empty\n")
-        else: 
-            try:
-                if float(self.mass) < 0:
-                    message.append("Mass must be greater than 0\n")
-            except:
-                pass
-        
-        if (self.dens == '' and check_density == True):
-            message.append("Density field is empty\n")        
-        
-        if message == []:
-            return True
-        else:
-            return False, message
-        
-    def error_message(self, messages):
-        err = 'Error:\n'
-        for message in messages:
-            err += message 
-        return err
-
-"""
