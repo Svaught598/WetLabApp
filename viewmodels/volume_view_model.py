09452b2a74f36d56fdd1bd02662a93d865c054d8
Svaught598 <svaught598@gmail.com>
2020-02-08 13:28:56 -0600

    added verification to volume viewmodel

--- viewmodels/volume_view_model.py
+++ viewmodels/volume_view_model.py
@@ -19,21 +19,23 @@ class VolumeViewModel(EventDispatcher):
     )
 
     solvents = ListProperty([])
-    volume_needed = NumericProperty()
+    volume_needed = StringProperty()
     error = StringProperty()
 
     def calculate(self, context):
         if self.verify_fields(context):
-            if context["solution_types"] == '%Wt/Wt':
+            # TODO: check these formulas again 
+            if context["solution_types"] == '% Wt/Wt':
                 concentration = float(context['concentration'])
                 mass = float(context['mass'])
                 density = float(context['density'])
                 self.volume_needed = str(round(((1 - concentration) * mass)/(concentration * density), 3)) + ' ml'   
-
-            elif context["solution_types"] == '%Wt/V':
+            # TODO: check these formulas again
+            elif context["solution_types"] == '% Wt/Vol':
+                print('we/v function')
                 concentration = float(context['concentration'])
                 mass = float(context['mass'])
-                self.volume_needed = str(round((mass/concentration), 3))
+                self.volume_needed = str(round((mass/concentration), 3)) + 'ml'
         else:
             return
 
@@ -43,27 +45,30 @@ class VolumeViewModel(EventDispatcher):
         
         # must check for every situation
         if context["solution_types"] == 'Solution Type':
-            error_message.join("Choose a Solution Type\n")
+            error_message += 'Choose a Solution Type\n'
         if context["solvent"] == 'Solvent':
-            error_message.join('Choose a Solvent\n')
+            error_message += 'Choose a Solvent\n'
         if context["material"] == 'Material':
-            error_message.join('Choose a Material\n')
-
-        # TODO: add checks for empty inputs
+            error_message += 'Choose a Material\n'
 
         # All other verifications depend on previous selections
-        if float(context["concentration"]) > 1:
-            error_message.join("Concentration must be less than 1\n")
-        if float(context["concentration"]) < 0:
-            error_message.join("Concentration must be greater than 0\n")
-        if float(context["mass"]) < 0:
-            error_message.join("Mass must be greater than 0\n")
+        try:
+            if float(context["concentration"]) > 1:
+                error_message += "Concentration must be less than 1\n"
+            elif float(context["concentration"]) < 0:
+                error_message += "Concentration must be greater than 0\n"
+        except ValueError:
+            error_message+="Concentration field empty\n"
+        try:
+            if float(context["mass"]) < 0:
+                error_message += "Mass must be greater than 0\n"
+        except ValueError:
+            error_message += "Mass field empty\n"
 
-        # TODO: Add special verifications for each option
-
-        self.error = error_message
-
-        if error_message == '':
-            return True
-        else:
-            return False
+        # Return True if no errors, False and errors if any
+        finally:
+            if error_message == '':
+                return True
+            else:
+                self.error = error_message
+                return False
