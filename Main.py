bda8b8d89403ebbfcb4479269812c8a7ce49dafa
Svaught598 <svaught598@gmail.com>
2020-02-09 14:37:28 -0600

    nav drawer construction in own method

--- Main.py
+++ Main.py
@@ -32,22 +32,8 @@ class SolutionApp(MDApp):
     def on_start(self):
         self.add_view_models()
         self.add_views()
+        self.add_nav_drawer()
         self.load_changes()
-        context = {
-            'Home': 'home-circle-outline',
-            'Settings': 'settings-outline',
-            'About': 'lambda',
-            'Developing': 'keyboard',
-            'Exit': 'exit-to-app',
-        }
-        for items in context.items():
-            self.root.ids.content_drawer.ids.box_item.add_widget(
-                NavigationItem(
-                text=items[0],
-                icon=items[1],
-                on_press= lambda x: self.get_main_screen()
-                )
-            )
 
     def add_views(self):
         self.screen1 = VolumeScreen(name = 'volumeneeded')
@@ -62,9 +48,38 @@ class SolutionApp(MDApp):
         self.update_view_model = UpdateViewModel()
         self.film_view_model = FilmViewModel()
 
+    def add_nav_drawer(self):
+        context = [
+            ['Home', 'home-circle-outline', self.get_main_screen()],
+            ['Settings', 'settings-outline', self.get_settings_screen()],
+            ['About', 'lambda', self.get_about_screen()],
+            ['Developing', 'keyboard', self.get_developing_screen()],
+            ['Exit', 'exit-to-app', self.exit_app()]
+        ]
+        for items in context:
+            print(items[2])
+            self.root.ids.content_drawer.ids.box_item.add_widget(
+                NavigationItem(
+                text=items[0],
+                icon=items[1],
+                on_press= lambda x: items[2]))
+        return 
+
     def get_main_screen(self):
         self.root.ids.screens.current = 'menu'
         return 
+
+    def get_settings_screen(self):
+        pass
+
+    def get_about_screen(self):
+        pass
+
+    def get_developing_screen(self):
+        pass
+
+    def exit_app(self):
+        pass
 
     def load_changes(self):
         self.SOLUTION_TYPES = [
