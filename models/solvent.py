62e508a89b55d003b0e25f7cb2a06813295a5d21
Svaught598 <svaught598@gmail.com>
2020-02-14 15:53:09 -0600

    added unique constraint to solvent name

--- models/solvent.py
+++ models/solvent.py
@@ -5,7 +5,7 @@ from .base import BaseModel
 class Solvent(BaseModel):
     '''Peewee Model describing solvent'''
     solventID = IntegerField(primary_key = True)
-    name = CharField()
+    name = CharField(unique = True)
     density = CharField()
     formula = CharField()
     polarity = CharFiel()
