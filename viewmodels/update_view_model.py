cda07aa0c6087e2a71cebfb3bf9cadfbf66171a2
Svaught598 <svaught598@gmail.com>
2020-02-15 22:49:31 -0600

    saving progress with commit

--- viewmodels/update_view_model.py
+++ viewmodels/update_view_model.py
@@ -1,6 +1,59 @@
 from kivy.event import EventDispatcher
 from kivy.clock import Clock
+from kivy.properties import StringProperty, ListProperty
+
+from models.solvent import Solvent
+
+import string
 
 
 class UpdateViewModel(EventDispatcher):
-    pass
+    
+    error = StringProperty('')
+    solvent_list = ListProperty()
+
+    def add_solvent(self, context):
+        print('solvent data added! ... not really though')
+        if self.check_solvent(context):
+            solvent = Solvent.create(
+                name = context['name'],
+                density = float(context['density']),
+                formula = context['formula'],
+                polarity = float(context['polarity']))
+            solvent.save()
+            print('it saved!')
+
+    def get_solvents(self):
+        solvent_list = []
+        for record in Solvent.select():
+            solvent = {
+                'name': record.name,
+                'density': record.density,
+                'formula': record.formula,
+                'polarity': record.polarity}
+            solvent_list.append(solvent)
+        self.solvent_list = solvent_list
+            
+
+    def check_solvent(self, context):
+        for key in context:
+            if context[key] == '':
+                self.error = "One or more fields empty!"
+                return False
+        if float(context['density']) < 0:
+            self.error = "Density must be positive!"
+            return False
+        elif float(context['polarity']) < 0:
+            self.error = "Polarity must be positive!"
+            return False
+        for char in context['formula']:
+            if char in string.whitespace:
+                self.error = "Invalid chemical formula"
+                return False
+            elif char in string.punctuation:
+                self.error = "Invalid chemcial formula"
+                return False
+        return True
+
+    def add_material(self, context):
+        print('material added! ... not really though')
--- views/update_view.py
+++ views/update_view.py
@@ -1,13 +1,19 @@
 from kivy.uix.screenmanager import Screen
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
 from kivy.properties import StringProperty
 from kivy.clock import Clock
+from kivy.metrics import dp
 
 from kivymd.uix.button import MDTextButton
 from kivymd.uix.tab import MDTabs, MDTabsBase
+from kivymd.uix.dialog import MDDialog
 from kivymd.app import MDApp
 
 from models.solvent import Solvent
+
+# class SolventViewClass(Label):
+#     pass
 
 
 class UpdateScreen(Screen):
@@ -31,16 +37,31 @@ class UpdateScreen(Screen):
         screen = NewMaterialScreen(name = 'Materials')
         self.manager.add_widget(screen)
 
+        # Binding view_model properties to view events
         app = MDApp.get_running_app()
-        app.update_view_model.bind() 
-        # TODO: bind view_model properties to view events above
+        app.update_view_model.bind(
+            error = lambda x, y: self.error_popup(y),
+            solvent_list = lambda x, y: self.refresh_solvent_rv(y)
+        ) 
+        app.update_view_model.get_solvents()
+
+    def refresh_solvent_rv(self, solvent_list):
+        for tab in self.ids.update_tabs.ids.scrollview.children[0].children:
+            if tab.state == 'down':
+                tab.
+
+    def error_popup(self, error):
+        screen = self.manager.current_screen
+        screen.error_popup(error)
 
     def plus_button_pressed(self):
+        # Screen manager navigation to appropriate 'new item' screen
         for tab in self.ids.update_tabs.ids.scrollview.children[0].children:
             if tab.state == 'down':
                 self.manager.current = tab.text
 
-    def on_exit(self):
+    def exit(self):
+        # remove screens when leaving
         for screen in self.manager.children:
             if screen.name == 'Solvents':
                 self.manager.remove_widget(screen)
@@ -56,6 +77,23 @@ class NewSolventScreen(Screen):
         app = MDApp.get_running_app()
         app.root.ids.screens.current = 'update'
 
+    def submit(self):
+        app = MDApp.get_running_app()
+        app.update_view_model.add_solvent({
+            'name': self.ids.name.text,
+            'density': self.ids.density.text,
+            'formula': self.ids.formula.text,
+            'polarity': self.ids.polarity.text,})
+
+    def error_popup(self, error):
+        self.dialog = MDDialog(
+            title = 'Error',
+            text = error,
+            size_hint = (0.8, None),
+            height = dp(200))
+        self.dialog.open()
+
+
 class NewMaterialScreen(Screen):
     
     def back(self):
@@ -70,3 +108,10 @@ class SolventTab(BoxLayout, MDTabsBase):
 class MaterialTab(BoxLayout, MDTabsBase):
     name = 'new_Materials'
     pass
+
+
+        
+# TODO: find way to push data to RV. trouble is in finding
+# a way to reference the RV from the parent screen.
+# probably need to bind a few properties to transport the data.
+# sucks, but that seems to be the only way.
