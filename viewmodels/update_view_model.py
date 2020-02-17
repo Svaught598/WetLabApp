a9760c35dcf1e5a667b56182b460d2537bee0e97
Svaught598 <svaught598@gmail.com>
2020-02-16 19:23:54 -0600

    added material model to updateViewModel

--- viewmodels/update_view_model.py
+++ viewmodels/update_view_model.py
@@ -6,6 +6,7 @@ from kivy.properties import (
     BooleanProperty)
 
 from models.solvent import Solvent
+from models.material import Material
 
 import string
 
@@ -18,39 +19,60 @@ class UpdateViewModel(EventDispatcher):
     error_added = BooleanProperty()
 
     def add_solvent(self, context):
-        print('solvent data added! ... not really though')
-        if self.check_solvent(context):
-            solvent = Solvent.create(
-                name = context['name'],
-                density = float(context['density']),
-                formula = context['formula'],
-                polarity = float(context['polarity']))
-            solvent.save()
-            self.error_added = False 
+        if self.check_solvent(context) == False:
+            return
+        solvent = Solvent.create(
+            name = context['name'],
+            density = float(context['density']),
+            formula = context['formula'],
+            polarity = float(context['polarity']))
+        solvent.save()
+        self.error_added = False 
         self.get_solvents()
+
+    def add_material(self, context):
+        if self.check_material(context) == False:
+            return
+        material = Material.create(
+            name = context['name'],
+            formula = context['formula'],
+            molecular_weight = context['molecular_weight'])
+        material.save()
+        self.error_added = False
+        self.get_materials()
 
     def get_solvents(self):
         self.solvent_list = Solvent.get_all()
+
+    def get_materials(self):
+        self.material_list = Material.get_all()
             
     def check_solvent(self, context):
         for key in context:
             if context[key] == '':
                 self.error = "One or more fields empty!"
                 return False
+        for char in context['formula']:
+            if (char in string.whitespace) or (char in string.punctuation):
+                self.error = "Invalid chemical formula"
+                return False
         if float(context['density']) < 0:
             self.error = "Density must be positive!"
             return False
-        elif float(context['polarity']) < 0:
+        if float(context['polarity']) < 0:
             self.error = "Polarity must be positive!"
             return False
-        for char in context['formula']:
-            if char in string.whitespace:
-                self.error = "Invalid chemical formula"
-                return False
-            elif char in string.punctuation:
-                self.error = "Invalid chemcial formula"
-                return False
         return True
 
-    def add_material(self, context):
-        print('material added! ... not really though')
+    def check_material(self, context):
+        for key in context:
+            if context[key] == '':
+                self.error = 'One or more fields empty!'
+                return False
+        for char in context['formula']:
+            if (char in string.whitespace) or (char in string.punctuation):
+                self.error = 'Invalid chemical formula'
+                return False
+        if float(context['molecular_weight']) < 0:
+            self.error = "Molecular weight must be positive!"
+            return False
