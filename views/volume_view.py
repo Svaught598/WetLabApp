356913c2a4a5c33b702bf2424aeba0ad8802e99e
Svaught598 <svaught598@gmail.com>
2020-02-21 09:36:07 -0600

    added dropdown for mass units

--- views/volume_view.py
+++ views/volume_view.py
@@ -14,7 +14,7 @@ from kivy.clock import Clock
 from kivymd import factory_registers
 
 from models.solvent import Solvent
-from settings import SOLUTION_TYPES
+from settings import SOLUTION_TYPES, MASS_UNITS
 
 
 class MDMenuItem(MDRectangleFlatButton):
@@ -23,8 +23,12 @@ class MDMenuItem(MDRectangleFlatButton):
 
 class VolumeScreen(Screen):
     SOLVENT_NAMES = ListProperty([])
-    SOLUTION_TYPES = ListProperty([])
     MATERIAL_NAMES = ListProperty([])
+
+    _MASS_UNITS = ListProperty([])
+    _SOLUTION_TYPES = ListProperty([])
+    _MASS_UNIT_DEFAULT = MASS_UNITS[0]
+    _SOLUTION_TYPE_DEFAULT = SOLUTION_TYPES[0]
 
     def __init__(self, *args, **kwargs):
         super(VolumeScreen, self).__init__(*args, **kwargs)
@@ -39,6 +43,12 @@ class VolumeScreen(Screen):
             solvent_list = lambda x, y: self.add_solvents(y),
             material_list = lambda x, y: self.add_materials(y)
         )
+        
+        # populate dropdowns from consts
+        self.add_solution_types(SOLUTION_TYPES)
+        self.add_mass_units(MASS_UNITS)
+
+        # populate dropdowns from database through viewmodel
         app.volume_view_model.get_solvents()
         app.volume_view_model.get_materials()
 
@@ -56,6 +66,9 @@ class VolumeScreen(Screen):
     def on_material(self, text):
         self.ids.material.text = text
 
+    def on_mass_unit(self, text):
+        self.ids.mass_units.text = text
+
     def calculate_button_pressed(self):
         app = MDApp.get_running_app()
         app.volume_view_model.calculate({
@@ -63,18 +76,19 @@ class VolumeScreen(Screen):
             'solvent': self.ids.solvent.text,
             'material': self.ids.material.text,
             'mass': self.ids.mass.text,
-            'concentration': self.ids.concentration.text
+            'concentration': self.ids.concentration.text,
+            'mass_unit': self.ids.mass_units.text,
         })
 
-    def add_solvents(self, solvents):
-        self.SOLVENT_NAMES = [{
+    def add_mass_units(self, units):
+        self._MASS_UNITS = [{
             'viewclass': 'MDMenuItem',
-            'text': solvent['name'],
-            'callback': self.on_solvent
-            } for solvent in solvents]
+            'text': unit,
+            'callback': self.on_mass_unit
+        } for unit in units]
 
     def add_solution_types(self, types):
-        self.SOLUTION_TYPES = [{
+        self._SOLUTION_TYPES = [{
             'viewclass': 'MDMenuItem',
             'text': type,
             'callback': self.on_solution_types
@@ -86,6 +100,13 @@ class VolumeScreen(Screen):
             'text': material['name'],
             'callback': self.on_material
         } for material in materials]
+
+    def add_solvents(self, solvents):
+        self.SOLVENT_NAMES = [{
+            'viewclass': 'MDMenuItem',
+            'text': solvent['name'],
+            'callback': self.on_material
+        } for solvent in solvents]
 
     def show_error_message(self, error_message):
         self.ids.volume_needed.text = error_message
