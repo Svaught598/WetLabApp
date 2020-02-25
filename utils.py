dfbf13d7257765b54a41ed731fc96399de3633ff
Svaught598 <svaught598@gmail.com>
2020-02-25 08:58:36 -0600

    fixed wrong conversions

--- utils.py
+++ utils.py
@@ -9,54 +9,54 @@ def convert_mass(mass, from_unit, to_unit):
     # from kg to ...
     if from_unit == MASS_UNITS[0]:
         if to_unit == MASS_UNITS[1]:
-            return mass * 1.e-3
+            return mass * 1.e3
         elif to_unit == MASS_UNITS[2]:
-            return mass * 1.e-6
+            return mass * 1.e6
         elif to_unit == MASS_UNITS[3]:
-            return mass * 1.e-9
+            return mass * 1.e9
         elif to_unit == MASS_UNITS[4]:
-            return mass * 1.e-12
+            return mass * 1.e12
 
     # from g to ...
     if from_unit == MASS_UNITS[1]:
         if to_unit == MASS_UNITS[0]:
-            return mass * 1.e3
-        elif to_unit == MASS_UNITS[2]:
             return mass * 1.e-3
+        elif to_unit == MASS_UNITS[2]:
+            return mass * 1.e3
         elif to_unit == MASS_UNITS[3]:
-            return mass * 1.e-6
+            return mass * 1.e6
         elif to_unit == MASS_UNITS[4]:
-            return mass * 1.e-9
+            return mass * 1.e9
 
     # from mg to ...
     if from_unit == MASS_UNITS[2]:
         if to_unit == MASS_UNITS[0]:
-            return mass * 1.e6
-        elif to_unit == MASS_UNITS[1]:
-            return mass * 1.e3
-        elif to_unit == MASS_UNITS[3]:
-            return mass * 1.e-3
-        elif to_unit == MASS_UNITS[4]:
             return mass * 1.e-6
+        elif to_unit == MASS_UNITS[1]:
+            return mass * 1.e-3
+        elif to_unit == MASS_UNITS[3]:
+            return mass * 1.e3
+        elif to_unit == MASS_UNITS[4]:
+            return mass * 1.e6
 
     # from ug to ...
     if from_unit == MASS_UNITS[3]:
         if to_unit == MASS_UNITS[0]:
-            return mass * 1.e9
+            return mass * 1.e-9
         elif to_unit == MASS_UNITS[1]:
-            return mass * 1.e6
+            return mass * 1.e-6
         elif to_unit == MASS_UNITS[2]:
-            return mass * 1.e3
-        elif to_unit == MASS_UNITS[4]:
             return mass * 1.e-3
+        elif to_unit == MASS_UNITS[4]:
+            return mass * 1.e3
 
     # from ng to ...
     if from_unit == MASS_UNITS[4]:
         if to_unit == MASS_UNITS[0]:
-            return mass * 1.e12
+            return mass * 1.e-12
         elif to_unit == MASS_UNITS[1]:
-            return mass * 1.e9
+            return mass * 1.e-9
         elif to_unit == MASS_UNITS[2]:
-            return mass * 1.e6
+            return mass * 1.e-6
         elif to_unit == MASS_UNITS[3]:
-            return mass * 1.e3
+            return mass * 1.e-3
